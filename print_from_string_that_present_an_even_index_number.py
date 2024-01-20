c9d9b4d3ea2cc5f0d1de96c764291501fd8fdcab
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-21 00:05:32 +0800

    Creating the code to print the index numbers and finalized the range part to print the index numbers

--- print_from_string_that_present_an_even_index_number.py
+++ print_from_string_that_present_an_even_index_number.py
@@ -8,3 +8,11 @@
 word = input('Insert what you want sheesh ')
 print("Original String:", word)
 
+# get the length of a string
+size = len(word)
+
+# Creating a code that will print the index_number
+print("Printing only even index chars")
+for i in range(0, size - 1, 2):
+    print("index_number[", i, "]", word[i])
+    
