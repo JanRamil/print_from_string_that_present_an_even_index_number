4f8c6c9f6615640507ea49fd9f7aa41fb0f02802
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-21 00:03:36 +0800

    Creating the starting code for print

--- print_from_string_that_present_an_even_index_number.py
+++ print_from_string_that_present_an_even_index_number.py
@@ -1,3 +1,10 @@
 # Print characters from a string that are present at an even index number
+# Exercise 3
 
 # pseudocode
+
+# Creating a starting code for print
+#for name in names
+word = input('Insert what you want sheesh ')
+print("Original String:", word)
+
